d67ec7297e789857d61b4caf4205c5cf26978986
sft <stf@example.com>
2024-05-11 16:23:38 +0800

    PCDto2D

---
+++ build/CMakeFiles/pcdtotxt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcdtotxt.dir/pcdtotxt.cpp.o"
+  "pcdtotxt"
+  "pcdtotxt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcdtotxt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
